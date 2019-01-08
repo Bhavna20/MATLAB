0e290c6ab44437f87101c89ebabd4f6e3e8d8257
Bhavna Joshi <46226206+Bhavna20@users.noreply.github.com>
2019-01-08 13:36:18 -0500

    Update vec.m

--- vec.m
+++ vec.m
@@ -1,8 +1,6 @@
 function [vecy]=vec(y)
 
-% Lutz Kilian
-% University of Pennsylvania
-% December 1993
+
 
 % This function vectorizes an (a x b) matrix y.  The resulting vector vecy
 % has dimension (a*b x 1).
